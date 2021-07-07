0fbcadfee1b1eed5ec57bda55a4f2f9ad700281b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-07 23:01:51 +0530

    add the object for the class VideoView

--- app/src/main/java/com/example/mp4player_app_java/MainActivity.java
+++ app/src/main/java/com/example/mp4player_app_java/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.mp4player_app_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
     Button playButton;
+    VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
